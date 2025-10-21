9b6acfc76a7b844a190af3b2b727d1b7383ab521
Catalina <catalinaelena08@yahoo.com>
2025-10-21 16:27:41 +0200

    router setup + create pages folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route} from 'react-router';
+import Login from "./pages/Login.jsx";
+import Feed from "./pages/Feed.jsx";
+import ActivityDetail from "./pages/ActivityDetail.jsx";
+import CreateActivity from "./pages/CreateActivity.jsx";
+import Profile from "./pages/Profile.jsx";
+//here we difine routes only
 
-function App() {
+
+export default function App() {
   return (
-      <p>
-        Bondi-app project works!
-      </p>
-  )
-}
+    <Routes> 
+      {/* Auth */}
+      <Route path="/login" element={<Login />} /> 
 
-export default App
+      {/* Main App */}
+      <Route path="/feed" element={<Feed />} />
+      <Route path="/activity/:id" element={<ActivityDetail />} />
+      <Route path="/create-activity" element={<CreateActivity />} />
+      <Route path="/profile/:username" element={<Profile />} />
+
+    </Routes>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Feed from"./pages/Feed";
+import Login from"./pages/Login"
+import ActivityDetail from "./pages/ActivityDetail";
+import CreateActivity from "./pages/CreateActivity";
+import Profile from"./pages/Profile"
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+
+const root = document.getElementById("root");
+const router = createBrowserRouter([
+  { path: "/feed", element: <Feed />, errorElement: <div>Something went wrong!</div>, },
+  { path: "/login", element: <Login />, errorElement: <div>Something went wrong!</div>, },
+  { path: "/activity/:id", element: <ActivityDetail />, errorElement: <div>Something went wrong!</div>, },
+  { path: "/create-activity", element: <CreateActivity />, errorElement: <div>Something went wrong!</div>, },
+  { path: "/profile/:username", element: <Profile />, errorElement: <div>Something went wrong!</div>, },
+]);
+
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />  
+  </React.StrictMode>,
+);
+
+//here is the only place when using ReactDOM.createRoot and wrap providers like the router.
