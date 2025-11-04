a8f5ee098b0ec4377ae3ff49a3ab5d28ecea3126
Catalina <catalinaelena08@yahoo.com>
2025-11-04 15:10:55 +0100

    uninstall new version react-router/install react-router-dom

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Feed from "./pages/Feed.jsx";
 import ActivityDetail from "./pages/ActivityDetail.jsx";
