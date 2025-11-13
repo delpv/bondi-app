b6771f3de591754193dc500f2220438d06868ae5
Catalina <catalinaelena08@yahoo.com>
2025-11-13 19:34:50 +0100

    fix parseConfig file

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,6 @@ import "./main.css";
 import { BrowserRouter, Routes } from "react-router-dom";
 import Router from "./Router.jsx";
 
-import Parse from "./utils/parseConfig"; // now Parse is initialized
-
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
